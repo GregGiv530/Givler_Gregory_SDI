d08aca08b1c24e620f67b95f3437be6f07884e43
GregGiv530 <gjgivler@fullsail.edu>
2015-03-05 14:54:48 -0500

    day 2 intro file starting
    first script sent to git hub

---
+++ Day 2/js/script.js
@@ -0,0 +1,20 @@
+/*
+Gregory Givler
+3/5/15
+Section 00
+Day 2 Intro
+ */
+
+//alert("Testing to see if connected.");
+
+/*
+Example of a multi-lined comment.
+Anything inside of here will not be seen by the computer.
+ */
+
+//Single line comment. Only this line will be ignored.
+
+//Alert box- pop up boc that will alert the user with information.
+alert("Text that the user will see.");
+
+
