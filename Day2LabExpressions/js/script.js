26b3c4c6c303289f81b94e8d34595b0655ca97a1
GregGiv530 <gjgivler@fullsail.edu>
2015-03-05 20:45:23 -0500

    Fixed grocery bill
    Had to correct the third problem.

--- Day2LabExpressions/js/script.js
+++ Day2LabExpressions/js/script.js
@@ -35,11 +35,24 @@ console.log("Sparky got "+leftovers+" slices of pizza.");
 Average Shopping Bill
  */
 
-//Number of weeks
-var weeks=5;
+//Random amount spent per week
+var week1=123;
+var week2=189;
+var week3=220;
+var week4=143;
+var week5=183;
 //Amount spent over 5 weeks
-var total=1500;
+var total=week1+week2+week3+week4+week5;
 //Average per week
-var average=total/weeks;
+var average=total/5;
 //Added the total spent over 5 weeks and the average per week
 console.log("You have spent a total of $"+total+" on groceries over 5 weeks. That is an average of $"+average+" per week");
+
+/*
+Discounts
+ */
+
+//Original Price
+var oPrice=800;
+//Discount Percentage
+var discount=.2;
