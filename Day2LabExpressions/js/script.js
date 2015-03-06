7124f81e1bd0f1dfd566a1731040da143a69315f
GregGiv530 <gjgivler@fullsail.edu>
2015-03-05 19:11:42 -0500

    Script Setup
    Gave variables to the pizzas, slices, and people at the party. Then
    multiplied the number of pizzas by how many slices each pizzas would
    have.

---
+++ Day2LabExpressions/js/script.js
@@ -0,0 +1,18 @@
+/*
+ Gregory Givler
+ 3/5/15
+ Section 00
+ Day 2 Lab Expressions
+  */
+
+alert("Day 2 Lab Expressions.")
+
+/*
+Slices of Pie 1
+ */
+
+var pizzas = 7;
+var people = 16;
+var slices = 8;
+var slicePerPerson = pizzas*slices;
+console.log(slicePerPerson);
