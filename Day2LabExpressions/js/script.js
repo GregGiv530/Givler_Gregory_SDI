048b69ae8e201a55cd6dcea6676b0e122d59e626
GregGiv530 <gjgivler@fullsail.edu>
2015-03-05 20:28:26 -0500

    Sparky and beginning of grocery bill
    Gave the total number of pieces sparky would get and started on the
    grocery bill problem

--- Day2LabExpressions/js/script.js
+++ Day2LabExpressions/js/script.js
@@ -11,12 +11,30 @@ alert("Day 2 Lab Expressions.")
 Slices of Pie 1
  */
 
+//Number of pizza
 var pizzas = 7;
-var people = 14;
+//Number of people
+var people = 16;
+//Number of slices per pizza
 var slices = 8;
-var slicesPerPizza = pizzas*slices;
-var slicesPerPerson = slicesPerPizza/people;
+//Slices per person
+var slicesPerPerson = (slices*pizzas)/people;
+//Adding the slices per person to the text sting
 console.log("Each person gets "+slicesPerPerson+" slices at the party.");
 
+/*
+Slice of Pie 2
+ */
 
+//Sparky's pieces of pizza
+var leftovers = slicesPerPizza%people;
+//Adding the total number of slices Sparky will get
+console.log("Sparky got "+leftovers+" slices of pizza.");
 
+/*
+Average Shopping Bill
+ */
+
+//Amount spent over 5 weeks
+var total=1500
+var average=
