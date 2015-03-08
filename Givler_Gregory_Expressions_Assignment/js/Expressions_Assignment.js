02e700c4c351b8eb5cabfb05604a56b497992b4d
GregGiv530 <gjgivler@fullsail.edu>
2015-03-08 18:43:44 -0400

    Test and made the alert do the intro.
    The alert introduces the calculator.

--- Givler_Gregory_Expressions_Assignment/js/Expressions_Assignment.js
+++ Givler_Gregory_Expressions_Assignment/js/Expressions_Assignment.js
@@ -1,11 +1,11 @@
 /*
  Gregory Givler
- 3/7/2015
+ 3/8/2015
  Section 00
  Lab Expressions
  */
 
-//alert("Hello!");
+alert("Wondering how much money you spend at the movies per year?");
 
 //Monthly Movie Calculator
 //This calculator will help the user figure out what the average monthly expenses are for going to the movies.
@@ -14,7 +14,7 @@
 var ticket_items=["ticket","popcorn","drinks","candy","alone","people",12];
 
 //Users will enter the amount of a single movie ticket.
-var movieTicket=prompt("How much do you spend in a year at the movies? \nHow much does a single movie "+ticket_items[0]+" cost?");
+var movieTicket=prompt("How much does a single movie "+ticket_items[0]+" cost?");
 console.log(movieTicket);
 
 //Users will enter if they go alone to the movies or with people.
@@ -29,7 +29,7 @@ console.log(movieItems);
 var amountGone=prompt("How many times a month do you go to the movies?");
 console.log(amountGone);
 
-//Math formula for amount spent on tickets per peron.
+//Math formula for amount spent on tickets per peron and variable conversion.
 movieTicket*=alone_people;
 var ticketCost=movieTicket;
 
