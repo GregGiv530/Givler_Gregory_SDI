0e58e51862b1b5c05c026a3af37c505e73aa9fca
GregGiv530 <gjgivler@fullsail.edu>
2015-03-08 16:14:54 -0400

    Started assignment and concept
    Made the concept of how much on average does someone spends in a year at
    the movies. Made array for text strings and added first prompt.

---
+++ Givler_Gregory_Expressions_Assignment/js/Expressions_Assignment.js
@@ -0,0 +1,20 @@
+/*
+ Gregory Givler
+ 3/7/2015
+ Section 00
+ Lab Expressions
+ */
+
+//alert("Hello!");
+
+//Monthly Movie Calculator
+//This calculator will help the user figure out what the average monthly expenses are for going to the movies.
+
+//Array for movie ticket and expenses text strings.
+var ticketItems=["ticket","popcorn","drinks","candy"];
+
+//Users will enter the amount of a single movie ticket.
+var movieTicket=prompt("How much do you spend in a year at the movies? \nHow much does a single movie "+ticketItems[0]+" cost?");
+console.log(movieTicket);
+
+
