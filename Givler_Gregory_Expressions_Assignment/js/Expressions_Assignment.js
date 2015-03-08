6ae9336a6075038fb35b959042a27ff551479141
GregGiv530 <gjgivler@fullsail.edu>
2015-03-08 16:49:49 -0400

    Added text prompts and implemented array
    Added text prompts and implemented array

--- Givler_Gregory_Expressions_Assignment/js/Expressions_Assignment.js
+++ Givler_Gregory_Expressions_Assignment/js/Expressions_Assignment.js
@@ -11,10 +11,20 @@
 //This calculator will help the user figure out what the average monthly expenses are for going to the movies.
 
 //Array for movie ticket and expenses text strings.
-var ticketItems=["ticket","popcorn","drinks","candy"];
+var ticket_items=["ticket","popcorn","drinks","candy","alone","people"];
 
 //Users will enter the amount of a single movie ticket.
-var movieTicket=prompt("How much do you spend in a year at the movies? \nHow much does a single movie "+ticketItems[0]+" cost?");
+var movieTicket=prompt("How much do you spend in a year at the movies? \nHow much does a single movie "+ticket_items[0]+" cost?");
 console.log(movieTicket);
 
+//Users will enter if they go alone to the movies or with people.
+var alone_people=prompt("Do you go to the movies "+ticket_items[4]+" or with "+ticket_items[5]+"? \n(If you go alone enter 1, if not enter \nthe number of people you go with.)");
+console.log(alone_people);
 
+//Users will enter the amount they spend on popcorn, drinks and candy.
+var movieItems=prompt("How much money would you spend on "+ticket_items[1]+", "+ticket_items[2]+" \nand "+ticket_items[3]+"?");
+console.log(movieItems);
+
+//Users will enter the number of times they go to the movies a month.
+var amountGone=prompt("How many times a month do you go to the movies?");
+console.log(amountGone);
