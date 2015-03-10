bec603d8c48029037fc4712a50d46706126ea5aa
GregGiv530 <gjgivler@fullsail.edu>
2015-03-10 17:01:31 -0400

    Learned how to spell ternary and what it is
    Learned how to spell ternary and what it is

--- Day 4/js/Day4Ternary.js
+++ Day 4/js/Day4Ternary.js
@@ -1,8 +1,45 @@
 /*
  Gregory Givler
- 3/7/2015
+ 3/10/2015
  Section 00
- Day 3 Prompts
+ Day 4 Ternary
  */
 
-alert("Hello!");
+//alert("Hello!");
+
+//If your gpa is over 2.0 then you can graduate
+//Create a variable for gpa
+var gpa=3.5;
+
+//Basic Conditional
+if(gpa>2.0){
+    console.log("You can gradute!");
+} else{
+    console.log("Work harder, you can not graduate!");
+}
+
+//(condition to test)? code to run if true:code to run if false
+//create a basic ternary function for our gpa
+(gpa>2.0)?console.log("You can gradute!"):console.log("Work harder, you can not graduate!");
+
+//If a child is under 10 then they must read green eggs and ham, otherwise they can read the time machine.
+
+//Create a variable for child age and for the book they have to read.
+var age=16
+var book;
+
+//Use a ternary to define the book.
+book=(age<10)? "Green Eggs and Ham":"The Time Machine";
+
+console.log("The kid should read "+book+".");
+
+//Value of book if basic conditional
+var bookBasic;
+
+if(age<10){
+    bookBasic="Green Eggs and Ham";
+} else{
+    bookBasic="The Time Machine";
+}
+console.log(bookBasic);
+
