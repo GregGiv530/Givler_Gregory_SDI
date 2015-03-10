2b24cdc6c3d9382fb24db1781bfdc583dc26fae5
GregGiv530 <gjgivler@fullsail.edu>
2015-03-10 19:01:53 -0400

    Added math formulas
    Added conversion math formulas for both Fahrenheit and Celsius

--- Day 4 Conditional Worksheet/js/Day4ConditionalsWorksheet.js
+++ Day 4 Conditional Worksheet/js/Day4ConditionalsWorksheet.js
@@ -14,11 +14,17 @@
 var userInput=prompt("Would you like to know how to convert between \nCelsius and Fahrenheit? \nPlease enter F or C to convert either or.");
 var fahText="F";
 var celText="C";
+
 //Prompts for number input to convert to Celsius and Fahrenheit.
-if(fahText){
+if(userInput==fahText){
     var fah=prompt("Now enter the number you wish to convert into Celsius.");
-} else if(celText){
+} else if(userInput==celText){
     var cel=prompt("Now enter the number you wish to convert into Fahrenheit.");
 } else{
     alert("Please enter F or C!!!");
 }
+//Variables and math formulas for converting Celsius to Fahrenheit
+var fahConCel=(fah-32)*5/9
+var celConFah=cel*9/5+32
+console.log(fahConCel);
+console.log(celConFah);
