2f9c6e9a5480623275b2dbdcefa333e47804badc
GregGiv530 <gjgivler@fullsail.edu>
2015-03-10 18:17:32 -0400

    Started Conditional Worksheet
    Made variables and testing ways to get the correct answer.

---
+++ Day 4 Conditional Worksheet/js/Day4ConditionalsWorksheet.js
@@ -0,0 +1,23 @@
+/*
+ Gregory Givler
+ 3/10/2015
+ Section 00
+ Day 4 Conditionals Worksheet
+ */
+
+//alert("Hello!");
+
+//Group 1
+//Celsius to Fahrenheit Converter
+
+//Variables for Fahrenheit and Celsius
+var userInput=prompt("Would you like to know what is what between Celsius and Fahrenheit. \nPlease enter the number followed by and F or C.");
+var fah=Number(userInput);
+var fahText="F";
+var cel=Number(userInput);
+var celText="C";
+if(userInput==fahText&&cel*){
+    console.log("Hello");
+} else{
+    console.log("Can not compute!");
+}
