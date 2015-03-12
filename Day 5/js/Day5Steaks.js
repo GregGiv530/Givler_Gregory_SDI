69e833d945f4f608fcbbfe2e5e019d479f6f3673
GregGiv530 <gjgivler@fullsail.edu>
2015-03-12 14:26:01 -0400

    Find the steak Doneness
    Using multiple else if statements

--- Day 5/js/Day5Steaks.js
+++ Day 5/js/Day5Steaks.js
@@ -17,3 +17,29 @@
  Medium well 150–155 °F
  Well done 160 °F+
  */
+
+//Create a variable for our steak temperature
+var steakTemp=prompt("What is the temperature of your steak?");
+
+//We have to test each of the cases above to see where our temp falls into.
+
+if(steakTemp<115){
+    console.log("Your steak is uncooked!");
+} else if(steakTemp<125){
+    console.log("Your steak is Extra-Rare or Blue.");
+} else if(steakTemp<130){
+    console.log("Your steak is Rare.");
+} else if(steakTemp<140){
+    console.log("Your steak is Medium-Rare.");
+} else if(steakTemp<150){
+    console.log("Your steak is Medium.");
+} else if(steakTemp<155){
+    console.log("Your steak is Medium Well.");
+} else if(steakTemp<160){
+    console.log("Your steak is Well Done.");
+} else{
+    console.log("Your steak is Burnt.");
+}
+
+
+
