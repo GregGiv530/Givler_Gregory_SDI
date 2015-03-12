80e2b16fe6c172651ad47e084d909ef61777eedf
GregGiv530 <gjgivler@fullsail.edu>
2015-03-12 19:43:45 -0400

    Concept and Started
    Started concept of calculator and began testing different conditionals

--- Givler_Gregory_Conditionals_Assignment/js/Conditionals_Assignment.js
+++ Givler_Gregory_Conditionals_Assignment/js/Conditionals_Assignment.js
@@ -7,3 +7,22 @@
 
 //alert("Hello!");
 
+//Calculator for figuring out how much is spent on video games in an entire year.
+//Array for user input
+var userArray=[]
+
+//User input for what systems they have bought.(text string)
+userArray[0]=prompt("Trying to figure out how much you spent on video games this year?\nDid you buy a system/s this year?\nPlease only enter one console name.");
+userArray[0]=userArray[0].toLowerCase();
+
+//Conditional for user's system or systems that they bought.
+if(userArray[0]!="xbox one"||userArray[0]!="xbox 1"){
+    var xboxOne=prompt("You entered "+userArray[0]+".\nIs this Correct?");
+    xboxOne=xboxOne.toLowerCase()
+    if(xboxOne!="yes"){
+        
+    }
+} else{
+    console.log("Whats the dealio!");
+}
+
