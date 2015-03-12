4ce46bd904ad8515ba414094dfd8b927e2740137
GregGiv530 <gjgivler@fullsail.edu>
2015-03-12 13:53:55 -0400

    Day 5 Steaks
    Day 5 steaks js file and html

---
+++ Day 5/js/Day5Steaks.js
@@ -0,0 +1,19 @@
+/*
+ Gregory Givler
+ 3/12/2015
+ Section 00
+ Day 5 Steaks
+ */
+
+//alert("Hello!");
+
+//Test a steak temperature and tell user the done-ness level
+
+/*
+ Extra-rare or Blue 115–120 °F
+ Rare 125–130 °F
+ Medium rare 130–140 °F
+ Medium 140–150 °F	145 °F
+ Medium well 150–155 °F
+ Well done 160 °F+
+ */
