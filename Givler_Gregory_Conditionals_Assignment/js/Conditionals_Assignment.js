940b0072ec49650e1d64646958e851635a3569c9
GregGiv530 <gjgivler@fullsail.edu>
2015-03-13 11:56:56 -0400

    Constructing conditionals
    Trying to figure out the best way to approach user input

--- Givler_Gregory_Conditionals_Assignment/js/Conditionals_Assignment.js
+++ Givler_Gregory_Conditionals_Assignment/js/Conditionals_Assignment.js
@@ -12,17 +12,30 @@
 var userArray=[]
 
 //User input for what systems they have bought.(text string)
-userArray[0]=prompt("Trying to figure out how much you spent on video games this year?\nDid you buy a system/s this year?\nPlease only enter one console name.");
+userArray[0]=prompt("Trying to figure out how much you spent on next-gen video games this year?\nDid you buy a system this year?");
 userArray[0]=userArray[0].toLowerCase();
+//Validation for user input
+if(userArray[0]==""){
+    userArray[0]=prompt("Please do not leave blank.\nDid you buy a system this year?");
+    userArray[0]=userArray[0].toLowerCase();
+} else if(userArray[0]=="no"){
+    console.log("This calculator is useless to you if you didn't buy any console this year!!!");
+}
 
 //Conditional for user's system or systems that they bought.
-if(userArray[0]!="xbox one"||userArray[0]!="xbox 1"){
-    var xboxOne=prompt("You entered "+userArray[0]+".\nIs this Correct?");
-    xboxOne=xboxOne.toLowerCase()
-    if(xboxOne!="yes"){
-        
+if(userArray[0]=="yes"){
+    userArray[1]=prompt("You said "+userArray[0]+".\nWhich console did you purchase?");
+    userArray[1]=userArray[1].toLowerCase();
+    //If user inputs Xbox One this code will run.
+    if(userArray[1]!="xbox one"||userArray[1]!="xbox 1"){
+        userArray[2]=prompt("How much did your Xbox One cost?");
+        //Validation of system cost
+        if(isNaN(userArray[2])){
+            userArray[2]=prompt("Please ONLY enter numbers.\nHow much did your Xbox One cost?");
+        }
+    //If user inputs Playstation 4 this code will run.
+    } else if(userArray[1]!="ps4"||userArray[1]!="playstation 4"){
+        userArray[2]=prompt("How much did your Playstation 4 cost?");
     }
-} else{
-    console.log("Whats the dealio!");
 }
 
