64a0a691e87734b0d11883246f2882c15757ca0e
GregGiv530 <gjgivler@fullsail.edu>
2015-03-14 00:38:46 -0400

    Tested all validations and fixed users output
    Tested all validations for correct user input and fixed the problem of
    user output not being a number

--- Givler_Gregory_Conditionals_Assignment/js/Conditionals_Assignment.js
+++ Givler_Gregory_Conditionals_Assignment/js/Conditionals_Assignment.js
@@ -85,27 +85,29 @@ if(userArray[0]=="yes"||userArray[0]=="Yes") {
 }
 //Adding in cost of sales tax for user's area or purchase for one system.
 userArray[6] = prompt("What is the sales tax percent in your area?\n(Please ONLY enter the number of the percent!)");
-userArray[6] /= 100;
-userArray[6] = Number(userArray[6]) * Number(userArray[2]);
-userArray[2] = Number(userArray[2]) + Number(userArray[6]);
 //Validation to make sure a number is entered.
 if (isNaN(userArray[6])) {
-    userArray[7] = prompt("Please ONLY use numbers!!!\nWhat is the sales tax percent in your area?");
-    userArray[7] /= 100;
-    userArray[7] = Number(userArray[7]) * Number(userArray[2]);
-    userArray[2] = Number(userArray[2]) + Number(userArray[7]);
+    userArray[6] = prompt("Please ONLY use numbers!!!\nWhat is the sales tax percent in your area?");
+    userArray[6] /= 100;
+    userArray[6] = Number(userArray[6]) * Number(userArray[2]);
+    userArray[2] = Number(userArray[2]) + Number(userArray[6]);
+}else{
+    userArray[6] /= 100;
+    userArray[6] = Number(userArray[6]) * Number(userArray[2]);
+    userArray[2] = Number(userArray[2]) + Number(userArray[6]);
 }
+
 //Output if user only buys one system
-//If user only buys a Xbox One
-    if (userArray[1] == "xbox one" || userArray[1] == "xbox 1" || userArray[1] == "xbox1") {
-        console.log("You said you want to buy a Xbox One and by adding in your sales tax you would spend $" + Number(userArray[2]) + " on your system.");
-        //If user only bought a Playstation 4
-    } else if (userArray[1] == "ps4" || userArray[1] == "playstation 4" || userArray[1] == "playstation4") {
-        console.log("You said you want to buy a Playstation 4 and by adding in your sales tax you would spend $" + Number(userArray[2]) + " on your system.");
-        //If user only bought a Wii
-    } else if (userArray[1] == "wii") {
-        console.log("You said you want to buy a Wii and by adding in your sales tax you would spend $" + Number(userArray[2]) + " on your system.");
-    }
+//If user buys a Xbox One
+if (userArray[1] == "xbox one" || userArray[1] == "xbox 1" || userArray[1] == "xbox1") {
+    console.log("You said you want to buy a Xbox One and by adding in your sales tax you would spend $" + (userArray[2]) + " on your system.");
+    //If user buys a Playstation 4
+} else if (userArray[1] == "ps4" || userArray[1] == "playstation 4" || userArray[1] == "playstation4") {
+    console.log("You said you want to buy a Playstation 4 and by adding in your sales tax you would spend $" + (userArray[2]) + " on your system.");
+    //If user buys a Wii
+} else if (userArray[1] == "wii") {
+    console.log("You said you want to buy a Wii and by adding in your sales tax you would spend $" + (userArray[2]) + " on your system.");
+}
 
 
 
