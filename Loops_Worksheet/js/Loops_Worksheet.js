ceeff0fb5ae37d55f1753b426f60b8bf912f6d49
GregGiv530 <gjgivler@fullsail.edu>
2015-03-14 19:00:49 -0400

    Completed While Loop
    Entered while loop that runs 7 time before its finished.

--- Loops_Worksheet/js/Loops_Worksheet.js
+++ Loops_Worksheet/js/Loops_Worksheet.js
@@ -6,3 +6,17 @@
  */
 
 //alert("Hello!");
+
+//While Loop
+//Variable for while loop
+var money=250
+var expenses=100
+
+//While Loop using the variable
+while(money<1250){
+    console.log("Almost enough to fix my laptop!!!")
+    money+=250
+    money-=expenses
+}
+//This while loop will run seven times before moving onto the next code
+
