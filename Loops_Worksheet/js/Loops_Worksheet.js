1f4eef2ff65c7d217fb7727204754499062b761d
GregGiv530 <gjgivler@fullsail.edu>
2015-03-14 19:21:28 -0400

    Do and for loops added
    examples for do loop and for loop were added

--- Loops_Worksheet/js/Loops_Worksheet.js
+++ Loops_Worksheet/js/Loops_Worksheet.js
@@ -9,14 +9,38 @@
 
 //While Loop
 //Variable for while loop
-var money=250
-var expenses=100
+var money=250;
+var expenses=100;
 
 //While Loop using the variable
-while(money<1250){
-    console.log("Almost enough to fix my laptop!!!")
+while((money-expenses)<1250){
+    console.log("Almost enough to fix my laptop!!!");
     money+=250
-    money-=expenses
 }
-//This while loop will run seven times before moving onto the next code
+//Loop will run four times before moving onto the next code
 
+//Do While Loop
+//Counting variables
+var i=0;
+var j=20;
+var k=80;
+
+//Do loop using variables
+do{
+    console.log("You now have $"+i+"!");
+    i+=k
+    j-=j
+}while(i<=600)
+//Loop will run eight times before stopping and moving onto the next line of code.
+
+//For Loop
+//Variable to use in for loop
+var l=2500;
+
+//for loop using variables
+for(var m=100;m<=l;m+=m){
+    console.log(m+" monkeys are juming up and down!!!");
+}
+//Loop will run five times before moving on.
+
+//Validation for text user input
