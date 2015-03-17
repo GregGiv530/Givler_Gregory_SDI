5fe5d3c36e12bed8811caacff6e78141288e3151
GregGiv530 <gjgivler@fullsail.edu>
2015-03-17 14:58:03 -0400

    Scooby Doo and Bills
    case solving and averages of bills

--- Day 7/js/Day7LoopArrays.js
+++ Day 7/js/Day7LoopArrays.js
@@ -6,3 +6,43 @@
  */
 
 //alert("Hello!");
+
+//Tell each member of the Scooby Gang that they solve the mystery
+
+//Create an array for gang members
+var names=["Scooby Doo","Shaggy","Velma","Daphne","Fred","Vincent Price"];
+
+//Add Scrappy to the gang
+names.push("Scrappy");
+
+//Create for loop to cycle through the array
+for(var i=0;i<names.length;i++){
+    //Console.log each person
+    console.log("You solved the case "+names[i]+"!");
+}
+
+//Keep track of our bills and get the total and average
+var bills=[50,10,5,20,10,60,70];
+
+//Create variables for total and average
+var total=0;
+var average=0;
+
+//Create a variable to track the item total
+var numItems=0;
+
+for(var j=0;j<bills.length;j++){
+    //Only add them if they are equal or over $15.
+    if(bills[j]>=15){
+        //Add each bill to the total
+        total+=bills[j];
+        numItems++
+    }
+}
+
+console.log("The total is "+total);
+
+//Calculate the average
+average=total/numItems;
+console.log("The average is "+average);
+
