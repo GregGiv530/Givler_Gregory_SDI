1dc54e279c77bbb2f31b6b0b3e885dd598b87a54
GregGiv530 <gjgivler@fullsail.edu>
2015-03-17 18:11:26 -0400

    Started Functions Worksheet
    Created js and html files for functions worksheet. Used function to
    produce the circumference of a circle with a radius of 10.

---
+++ Functions_Worksheet/js/Functions_Worksheet.js
@@ -0,0 +1,23 @@
+/*
+ Gregory Givler
+ 3/17/2015
+ Section 00
+ Functions Worksheet
+ */
+
+//alert("Hello!");
+
+//Circumference of a circle
+//Function for finding the circumference of a circle
+function circCircle(r){
+    circumference=2*Math.PI*r
+    return circumference
+}
+
+//Variable to catch the return
+var circResults=circCircle(10);
+
+//Console.log the answer
+console.log("The circumference of the circle is "+circResults+".");
+
+//Test values show the correct answer is show in the console. Function does the calculation.
