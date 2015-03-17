65b38ae1f0ccfff22759bb3bc687b73a9054d54e
GregGiv530 <gjgivler@fullsail.edu>
2015-03-17 13:52:49 -0400

    Bottles of beer on the wall
    The song in a loop with conditional

--- Day 7/js/Day7Beer.js
+++ Day 7/js/Day7Beer.js
@@ -5,4 +5,19 @@
  Day 7 Beer
  */
 
-alert("Hello!");
+//alert("Hello!");
+
+//Code the song 99 Bottles of beer on the wall.
+
+//Basic structure of for loop.
+//for(var i=0; condition to test; increment of change){}
+
+for(var i=99;i>0;i--) {
+    if(i===1){
+        console.log("1 bottle of beer on the wall. 1 bottle of beer. Take one down and pass it around. No more bottles of beer on the wall.\nThe end.")
+    } else{
+        console.log(i+" bottles of beer on the wall. "+i+" bottles of beer. Take one down and pass it around. "+(i-1)+" bottles of beer on the wall.")
+    }
+}
+
+
