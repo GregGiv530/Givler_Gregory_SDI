ce1f9d14c58b9c04ad56a12c8177870b1a8f3594
GregGiv530 <gjgivler@fullsail.edu>
2015-03-24 18:16:12 -0400

    Finished user prompts and added basic function
    user is prompted for cost of each piece of equipment and function adds
    up the values and returns the value

--- Givler_Gregory_Functions_Assignment/js/Givler_Gregory_Functions_Assignment.js
+++ Givler_Gregory_Functions_Assignment/js/Givler_Gregory_Functions_Assignment.js
@@ -7,13 +7,13 @@
 
 //alert("Hello!");
 
-//Calculator for how much a custom skateboard would cost.
+//Calculator for how much a custom skateboard would cost with a discount on the purchase.
 
 //Prompt for user input for cost of board
 var board=prompt("Let's find out how much it would cost to get a custom skateboard.\nHow much does your board cost?");
 //While loop for validation
 while(board===""||isNaN(board)){
-    board=prompt("Please do not leave blank and only use numbers.\nHow much does your board cost?")
+    board=prompt("Please do not leave blank and only use numbers.\nHow much does your board cost?");
 }
 //parseInt for user input
 board=parseInt(board);
@@ -22,7 +22,31 @@ board=parseInt(board);
 var trucks=prompt("How much do your trucks cost?");
 //While loop for validation
 while(trucks===""||isNaN(trucks)){
-    trucks=prompt("Please do not leave blank and only use numbers.\nHow much do your trucks cost?")
+    trucks=prompt("Please do not leave blank and only use numbers.\nHow much do your trucks cost?");
 }
 //parseInt for user input
 trucks=parseInt(trucks);
+
+//Prompt for user input for cost of trucks
+var bearings=prompt("How much do your bearings cost?");
+//While loop for validation
+while(bearings===""||isNaN(bearings)){
+    trucks=prompt("Please do not leave blank and only use numbers.\nHow much do your bearings cost?");
+}
+//parseInt for user input
+bearings=parseInt(bearings);
+
+//Prompt for user input for cost of trucks
+var wheels=prompt("How much do your wheels cost?");
+//While loop for validation
+while(wheels===""||isNaN(wheels)){
+    wheels=prompt("Please do not leave blank and only use numbers.\nHow much do your wheels cost?");
+}
+//parseInt for user input
+wheels=parseInt(wheels);
+
+//Function to add cost of board and trucks
+function costSf(board,trucks,bearings,wheels){
+    var i=board+trucks+bearings+wheels;
+    return i;
+}
