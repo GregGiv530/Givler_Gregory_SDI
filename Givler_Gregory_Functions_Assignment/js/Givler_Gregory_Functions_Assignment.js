79cac46fe2b7bedc8d326fc2a40cd7f515c78e07
GregGiv530 <gjgivler@fullsail.edu>
2015-03-24 18:01:54 -0400

    Concept and User prompts added
    developed concept for cost of a custom skate board and added two user
    prompts for board cost and truck cost

--- Givler_Gregory_Functions_Assignment/js/Givler_Gregory_Functions_Assignment.js
+++ Givler_Gregory_Functions_Assignment/js/Givler_Gregory_Functions_Assignment.js
@@ -6,3 +6,23 @@
  */
 
 //alert("Hello!");
+
+//Calculator for how much a custom skateboard would cost.
+
+//Prompt for user input for cost of board
+var board=prompt("Let's find out how much it would cost to get a custom skateboard.\nHow much does your board cost?");
+//While loop for validation
+while(board===""||isNaN(board)){
+    board=prompt("Please do not leave blank and only use numbers.\nHow much does your board cost?")
+}
+//parseInt for user input
+board=parseInt(board);
+
+//Prompt for user input for cost of trucks
+var trucks=prompt("How much do your trucks cost?");
+//While loop for validation
+while(trucks===""||isNaN(trucks)){
+    trucks=prompt("Please do not leave blank and only use numbers.\nHow much do your trucks cost?")
+}
+//parseInt for user input
+trucks=parseInt(trucks);
