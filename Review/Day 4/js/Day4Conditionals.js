7186b060900d0010b92e7d20b9a1007ce3cd7864
GregGiv530 <gjgivler@fullsail.edu>
2015-03-27 21:05:43 -0400

    Day 4 Conditionals Updated
    added user prompts and while loops

--- Review/Day 4/js/Day4Conditionals.js
+++ Review/Day 4/js/Day4Conditionals.js
@@ -30,14 +30,18 @@ if(oldEnough){
 //Relational operators
 
 //If the kid is over 48 inches tall, then he can ride the coaster
-var kidHeight=57;
-
+var kidHeight=prompt("You must be 48 inches tall to ride this coaster.\nHow tall are you?");
+while(kidHeight===""||isNaN(kidHeight)){
+    kidHeight=prompt("Please do not leave blank and only use numbers.\nHow tall are you?");
+}
 //Create a variable for the minimal height requirements.
 var minHeight=48;
 
 //Create a variable for sneaker lifts
-var sneakerLifts=2;
-
+var sneakerLifts=prompt("Are you using sneaker lifts to make you taller?\nIf so, how many inches do they add to your height?");
+while(sneakerLifts===""||isNaN(sneakerLifts)){
+    sneakerLifts=prompt("Please do not leave blank and only use numbers.\nHow many inches do the sneakers add?");
+}
 //test the kids height
 if(kidHeight>minHeight){
     console.log("Kid is tall enough to ride the coaster!");
