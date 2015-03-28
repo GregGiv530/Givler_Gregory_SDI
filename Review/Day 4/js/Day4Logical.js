492dc10c9188bfacdcd31f1e04024b199d131059
GregGiv530 <gjgivler@fullsail.edu>
2015-03-27 21:33:00 -0400

    Updated Day 4 Logical
    added user prompts, a function, and a conditional.

--- Review/Day 4/js/Day4Logical.js
+++ Review/Day 4/js/Day4Logical.js
@@ -11,10 +11,33 @@
 //Basic conditional, if we have enough money to buy a car
 
 //Create a few variables
-var budget=25000;
-var carPrice=23000;
-var payCheck=1600;
+var budget=prompt("Let's see if you have enough cash to buy a car.\nWhat is your budget for the car?");
+while (budget===""||isNaN(budget)){
+    budget=prompt("Please do not leave blank and only use numbers.\nWhat is your budget for the car?");
+}
+var carPrice=prompt("How much does the car cost?");
+while (carPrice===""||isNaN(carPrice)){
+    carPrice=prompt("Please do not leave blank and only use numbers.\nHow much does the car cost?");
+}
+var payCheck=prompt("How much is your paycheck?");
+while (payCheck===""||isNaN(payCheck)){
+    payCheck=prompt("Please do not leave blank and only use numbers.\nHow much is your paycheck?");
+}
+var cashAmount=function(b,p){
+    var amount=b+p;
+    return amount;
+}
+var totalCash=cashAmount(Number(budget),Number(payCheck));
+console.log(totalCash);
+if (budget>=carPrice){
+    console.log("Your budget is already high enough to buy the car you want!");
+} else if(totalCash>=Number(carPrice)){
+    console.log("With the help of your paycheck you can buy the car!");
+} else{
+    console.log("Sorry you are to broke to buy the car.")
+}
 
+/*
 if(budget>=carPrice){
     console.log("You can afford your car!");
 } else{
@@ -47,9 +70,10 @@ f && f = f
  */
 
 //You can buy a car if the budget is greater than or equal to the car price AND your paycheck is over 1500
-
+/*
 if(budget>=carPrice&&payCheck>1500){
     console.log("You can buy the car!");
 } else{
     console.log("Pick a cheaper car or find a better job!");
 }
+*/
